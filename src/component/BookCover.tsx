a15da523b0a90d9c08a51c58b22e07477b88748a
CarlesRojas <carlesrojas@outlook.com>
2024-06-22 12:21:06 +0200

    missing cover

--- src/component/BookCover.tsx
+++ src/component/BookCover.tsx
@@ -5,6 +5,7 @@ import { cn, getBiggestBookImage } from "@/util";
 import { Link } from "next-view-transitions";
 import Image from "next/image";
 import { AnchorHTMLAttributes, ReactNode, forwardRef, useMemo, useRef, useState } from "react";
+import { LuBook } from "react-icons/lu";
 
 export interface Props extends AnchorHTMLAttributes<HTMLAnchorElement> {
     book: Book;
@@ -15,7 +16,6 @@ const BookCover = forwardRef<HTMLAnchorElement, Props>(({ book, href, maxWidth, 
     const biggestImage = useRef(book && getBiggestBookImage(book));
 
     const scaledImage = useMemo(() => {
-        // TODO Get missing image
         if (!biggestImage.current) return null;
 
         const imageParams = new URLSearchParams(biggestImage.current.split("?")[1]);
@@ -78,6 +78,36 @@ const BookCover = forwardRef<HTMLAnchorElement, Props>(({ book, href, maxWidth, 
                     alt={book.title}
                     onError={() => setSrc(biggestImage.current)}
                 />
+            )}
+
+            {!src && (
+                <div
+                    className={cn(
+                        "absolute inset-0 -z-10 h-full w-full select-none bg-neutral-200 object-cover object-center blur-md transition-opacity dark:bg-neutral-800",
+                        !href && "opacity-100 dark:opacity-100",
+                        href &&
+                            "opacity-0 group-hover:opacity-100 group-focus:opacity-100 dark:group-hover:opacity-100 dark:group-focus:opacity-100",
+                    )}
+                    style={{ viewTransitionName: `bookCover-blur-${book.id}` }}
+                />
+            )}
+
+            {!src && (
+                <div
+                    className={cn(
+                        "flex h-full w-full select-none flex-col items-center justify-center gap-1 rounded-xl border border-neutral-500/10 bg-neutral-150 object-cover object-center p-3 transition-transform dark:bg-neutral-850",
+                        href && "mouse:group-hover:scale-[1.02] mouse:group-focus:scale-[1.02]",
+                    )}
+                    style={{ viewTransitionName: `bookCover-${book.id}` }}
+                >
+                    <LuBook className={cn("mb-2 size-8 min-h-8 min-w-8 stroke-2", !href && "size-16 min-h-16 min-w-16 stroke-2")} />
+
+                    {href && <h3 className="text-center font-bold leading-snug tracking-wide">{book.title}</h3>}
+
+                    {href && book.authors && book.authors.length > 0 && (
+                        <p className="text-center text-sm font-semibold leading-snug tracking-wide opacity-50">{book.authors[0]}</p>
+                    )}
+                </div>
             )}
         </>,
     );

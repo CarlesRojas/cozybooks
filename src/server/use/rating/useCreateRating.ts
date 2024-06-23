a41d1d668342d65734e62da3b8569948387cc47b
CarlesRojas <carlesrojas@outlook.com>
2024-06-23 15:47:51 +0200

    rating optimistic

--- src/server/use/rating/useCreateRating.ts
+++ src/server/use/rating/useCreateRating.ts
@@ -1,5 +1,7 @@
 import { createRating } from "@/server/action/rating";
 import { getClientSide } from "@/server/use/useUser";
+import { VolumesResult } from "@/type/Book";
+import { LibraryType } from "@/type/Library";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 interface Props {
@@ -25,17 +27,24 @@ export const useCreateRating = () => {
             const previousData: number | undefined = queryClient.getQueryData(["rating", bookId]);
             queryClient.setQueryData(["rating", bookId], rating);
 
-            // TODO optimistic update for rating inside book
+            await queryClient.cancelQueries({ queryKey: ["libraryBooks", LibraryType.FINISHED] });
+            const previousFinishedData: VolumesResult | undefined = queryClient.getQueryData(["libraryBooks", LibraryType.FINISHED]);
+            if (previousFinishedData) {
+                const newItems = previousFinishedData.items.map((item) => (item.id === bookId ? { ...item, rating: [{ rating }] } : item));
+                queryClient.setQueryData(["libraryBooks", LibraryType.FINISHED], { ...previousFinishedData, items: newItems });
+            }
 
-            return { previousData };
+            return { previousData, previousFinishedData };
         },
 
         onError: (error, { bookId }, context) => {
             context && queryClient.setQueryData(["rating", bookId], context.previousData);
+            context && queryClient.setQueryData(["libraryBooks", LibraryType.FINISHED], context.previousFinishedData);
         },
 
         onSettled: (data, error, { bookId }) => {
             queryClient.invalidateQueries({ queryKey: ["rating", bookId] });
+            queryClient.invalidateQueries({ queryKey: ["libraryBooks", LibraryType.FINISHED] });
         },
     });
 };
--- src/server/use/rating/useDeleteRating.ts
+++ src/server/use/rating/useDeleteRating.ts
@@ -1,5 +1,7 @@
 import { deleteRating } from "@/server/action/rating";
 import { getClientSide } from "@/server/use/useUser";
+import { VolumesResult } from "@/type/Book";
+import { LibraryType } from "@/type/Library";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 interface Props {
@@ -24,17 +26,24 @@ export const useDeleteRating = () => {
             const previousData: number | undefined = queryClient.getQueryData(["rating", bookId]);
             queryClient.setQueryData(["rating", bookId], null);
 
-            // TODO optimistic update for rating inside book
+            await queryClient.cancelQueries({ queryKey: ["libraryBooks", LibraryType.FINISHED] });
+            const previousFinishedData: VolumesResult | undefined = queryClient.getQueryData(["libraryBooks", LibraryType.FINISHED]);
+            if (previousFinishedData) {
+                const newItems = previousFinishedData.items.map((item) => (item.id === bookId ? { ...item, rating: [] } : item));
+                queryClient.setQueryData(["libraryBooks", LibraryType.FINISHED], { ...previousFinishedData, items: newItems });
+            }
 
-            return { previousData };
+            return { previousData, previousFinishedData };
         },
 
         onError: (error, { bookId }, context) => {
             context && queryClient.setQueryData(["rating", bookId], context.previousData);
+            context && queryClient.setQueryData(["libraryBooks", LibraryType.FINISHED], context.previousFinishedData);
         },
 
         onSettled: (data, error, { bookId }) => {
             queryClient.invalidateQueries({ queryKey: ["rating", bookId] });
+            queryClient.invalidateQueries({ queryKey: ["libraryBooks", LibraryType.FINISHED] });
         },
     });
 };
